c9819558d659fcbfbd8f03e77b8867c4d92da74b
Anas-AbdelRaoof <anasraoof2012@gmail.com>
2026-07-04 20:15:00 +0300

    Create run.py

---
+++ run.py
@@ -0,0 +1,11 @@
+from write_code import write_ai_generated_code
+from file_helpers import argv_length
+
+
+def run():
+    """
+    Connects all parts of code and writes the code in your file
+    """
+
+    argv_length()
+    write_ai_generated_code()
